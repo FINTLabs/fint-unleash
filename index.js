d3190e52e81a460df9ede3f10d7c4ecf17de05f4
Frode Sjovatsen <frode@fintlabs.no>
2022-04-09 22:19:45 +0200

    added azure ad auth

--- index.js
+++ index.js
@@ -1,77 +1,15 @@
 'use strict';
 
 const unleash = require('unleash-server');
-const passport = require('@passport-next/passport');
-const GoogleOAuth2Strategy = require('@passport-next/passport-google-oauth2');
+const azureAdminOauth = require("./azure-auth-hook");
 
-const googleClientId = process.env.GOOGLE_CLIENT_ID || 'clientId';
-const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET || 'clientSecret';
-const googleCallbackUrl = process.env.GOOGLE_CALLBACK_URL || 'http://localhost:4242/api/auth/callback';
-
-passport.use(
-    'google',
-    new GoogleOAuth2Strategy({
-            clientID: googleClientId,
-            clientSecret: googleClientSecret,
-            callbackURL: googleCallbackUrl,
-        },
-        (accessToken, refreshToken, profile, cb) => {
-            cb(
-                null,
-                new unleash.User({
-                    name: profile.displayName,
-                    email: profile.emails[0].value,
-                }),
-            );
-        },
-    ),
-);
-
-function googleAdminAuth(app) {
-    app.use(passport.initialize());
-    app.use(passport.session());
-    passport.serializeUser((user, done) => done(null, user));
-    passport.deserializeUser((user, done) => done(null, user));
-
-    app.get(
-        '/api/admin/login',
-        passport.authenticate('google', {
-            scope: ['email']
-        }),
-    );
-    app.get(
-        '/api/auth/callback',
-        passport.authenticate('google', {
-            failureRedirect: '/api/admin/error-login',
-        }),
-        (req, res) => {
-            res.redirect('/');
-        },
-    );
-
-    app.use('/api/admin/', (req, res, next) => {
-        if (req.user) {
-            next();
-        } else {
-            return res
-                .status('401')
-                .json(
-                    new unleash.AuthenticationRequired({
-                        path: '/api/admin/login',
-                        type: 'custom',
-                        message: `You have to identify yourself in order to use Unleash. Click the button and follow the instructions.`,
-                    }),
-                )
-                .end();
-        }
-    });
-}
 
 const options = {
-    enableLegacyRoutes: false,
-    adminAuthentication: 'custom',
-    preRouterHook: googleAdminAuth,
-};
+    authentication: {
+      type: 'custom',
+      customAuthHandler: azureAdminOauth,
+    },
+  };
 
 unleash.start(options).then(instance => {
     console.log(
